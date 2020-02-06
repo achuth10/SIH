cacd84fb706dd26e7edf240efc15cf7168e7ca61
achuth10 <43288837+achuth10@users.noreply.github.com>
2020-02-06 10:22:06 +0530

    Bug fixes

--- app/src/main/java/com/example/sih/Acitvities/MainActivity.java
+++ app/src/main/java/com/example/sih/Acitvities/MainActivity.java
@@ -19,9 +19,11 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.sih.Models.Constants;
 import com.example.sih.Models.MySingleton;
 import com.example.sih.R;
@@ -29,24 +31,19 @@ import com.example.sih.R;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.example.sih.Models.Constants.DB_URL;
+import static com.example.sih.Models.Constants.aws;
+import static com.example.sih.Models.Constants.compute;
+
 public class MainActivity extends AppCompatActivity implements LocationListener {
     protected LocationManager locationManager;
     TextView txtLat;
-    private Button gotomap;
-    //private String url = "https://sh935gbgj4.execute-api.us-east-1.amazonaws.com/prod/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtLat = findViewById(R.id.textview);
-        gotomap=findViewById(R.id.GoToMap);
-        gotomap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MapsActivity.class));
-            }
-        });
         locationManager = (LocationManager)
                 getSystemService(Context.LOCATION_SERVICE);
 
@@ -59,62 +56,20 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 5000, 0, this);
 
 
-        //AWS
-//        // Create an instance of CognitoCachingCredentialsProvider
-//        CognitoCachingCredentialsProvider cognitoProvider = new CognitoCachingCredentialsProvider(
-//                this.getApplicationContext(), "identity-pool-id", Regions.US_WEST_2);
-//
-//// Create LambdaInvokerFactory, to be used to instantiate the Lambda proxy.
-//        LambdaInvokerFactory factory = new LambdaInvokerFactory(this.getApplicationContext(),
-//                Regions.US_WEST_2, cognitoProvider);
-//
-//// Create the Lambda proxy object with a default Json data binder.
-//// You can provide your own data binder by implementing
-//// LambdaDataBinder.
-//        final MyInterface myInterface = factory.build(MyInterface.class);
-//
-//        RequestClass request = new RequestClass("John", "Doe");
-//// The Lambda function invocation results in a network call.
-//// Make sure it is not called from the main thread.
-//        new AsyncTask<RequestClass, Void, ResponseClass>() {
-//            @Override
-//            protected ResponseClass doInBackground(RequestClass... params) {
-//                // invoke "echo" method. In case it fails, it will throw a
-//                // LambdaFunctionException.
-//                try {
-//                    return myInterface.AndroidBackendLambdaFunction(params[0]);
-//                } catch (LambdaFunctionException lfe) {
-//                    Log.e("Tag", "Failed to invoke echo", lfe);
-//                    return null;
-//                }
-//            }
-//
-//            @Override
-//            protected void onPostExecute(ResponseClass result) {
-//                if (result == null) {
-//                    return;
-//                }
-//
-//                // Do a toast
-//                Toast.makeText(MainActivity.this, result.getGreetings(), Toast.LENGTH_LONG).show();
-//            }
-//        }.execute(request);
-
-
-        //
-
     }
 
 
     public void sendData(final Location loc)
     {
 
-
-        StringRequest request = new StringRequest(Request.Method.POST, Constants.DB_URL, new Response.Listener<String>() {
+        //String url = DB_URL+"?x="+loc.getLatitude()+"&y="+loc.getLongitude();
+       // System.out.println(url);
+        final StringRequest request = new StringRequest(Request.Method.POST, aws, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                //System.out.println("Request is "+request.toString());
                 System.out.println("Response is " + response.toString());
-                Toast.makeText(getApplicationContext(),response.toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),response.toString(), Toast.LENGTH_SHORT).show();
 
             }
         }, new Response.ErrorListener() {
@@ -131,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 Map <String,String> params  = new HashMap<String,String>();
                 params.put(Constants.X,String.valueOf(loc.getLatitude()));
                 params.put(Constants.y,String.valueOf(loc.getLongitude()));
+                //params.put("Content-Type", "text/plain");
                 //params.put(Constants.ID, String.valueOf(1));
                 return params;
             }
@@ -158,15 +114,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
 
 
-
-
     @Override
     public void onLocationChanged(Location location) {
         txtLat = findViewById(R.id.textview);
 
         txtLat.setText("Latitude:" +
-                location.getLatitude() +
-                ", Longitude:" +
+                location.getLatitude() + "\n" +
+                "Longitude:" +
                 location.getLongitude());
        sendData(location);
         //Toast.makeText(getApplicationContext(),"Data sent",Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/sih/Models/Constants.java
+++ app/src/main/java/com/example/sih/Models/Constants.java
@@ -3,10 +3,11 @@ package com.example.sih.Models;
 public class Constants {
 
     public  static final String DB_URL = "https://p7kb2zye28.execute-api.us-east-1.amazonaws.com/prod/lambda";
-    public  static final String X = "X";
-    public  static final String y = "Y";
+    public  static final String X = "x";
+    public  static final String y = "y";
     public  static final String ID = "ID";
     public  static final String DB1_URL = "https://sihteam2020.000webhostapp.com/";
     public  static final String compute = DB1_URL+"Compute.php";
+    public  static final String aws = DB1_URL+"aws.php";
 
 }
